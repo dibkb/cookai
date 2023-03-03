1a7eec4da6d4455635fa403d0583f088d58cf574
dibkb <dibas9110@gmail.com>
2023-03-03 09:59:06 +0530

    adding custom fonts and colors to tailwind config

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        satisfy: ["Satisfy", "cursive"],
+      },
+      colors: {
+        cookLime: "#C5FA2E",
+        cookLightYellow: "#F0FF92",
+        cookGreen: "#79B100",
+        cookDarkGreen: "#00652C",
+        cookTextDark: "#383838",
+        cookTextLight: "#656565",
+        cookDarkBg: "#1B1B1B",
+        cookGrayBg: "#CCCCCC",
+        cookLightBg: "#E0E0E0",
+      },
+    },
+  },
+  plugins: [],
+};
