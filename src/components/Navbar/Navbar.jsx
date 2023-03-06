8ec01cfa667c45578d117d0c047251d9f8e1cb13
dibkb <dibas9110@gmail.com>
2023-03-06 21:04:24 +0530

    hamburger transition

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import { navItemsList } from "../../utils/navitems";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 const Navbar = () => {
+  const [showMobile, setShowMobile] = useState(false);
   const navItems = navItemsList.map(({ id, item }) => {
     return (
       <nav
@@ -13,10 +15,19 @@ const Navbar = () => {
     );
   });
   return (
-    <nav className="fixed top-0 w-full px-4 lg:px-12 xl:px-24 2xl:px-64">
+    <nav className="z-10 fixed top-0 w-full px-4 lg:px-12 xl:px-24 2xl:px-64">
       <div className="flex items-center justify-between mt-3">
         <img src={logo} alt="" width={97} className="select-none" />
-        <div className="flex gap-10">{navItems}</div>
+        <div className="hidden gap-10 md:flex">{navItems}</div>
+        {/* navbar on smaller screens */}
+        <div className=" md:hidden">
+          <Bars3Icon
+            className={`h-8 w-8 cursor-pointer text-zinc-900 transition ease-in-out ${
+              showMobile ? "rotate-180" : "rotate-0"
+            }`}
+            onClick={() => setShowMobile((val) => !val)}
+          />
+        </div>
       </div>
     </nav>
   );
