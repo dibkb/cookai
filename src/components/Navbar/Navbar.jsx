cb679c0d85624fb149b14f51cc567868945898f4
dibkb <dibas9110@gmail.com>
2023-03-06 21:11:10 +0530

    rensopsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,11 +16,11 @@ const Navbar = () => {
   });
   return (
     <nav className="z-10 fixed top-0 w-full px-4 lg:px-12 xl:px-24 2xl:px-64">
-      <div className="flex items-center justify-between mt-3">
+      <div className="flex items-center justify-between mt-3 relative">
         <img src={logo} alt="" width={97} className="select-none" />
         <div className="hidden gap-10 md:flex">{navItems}</div>
         {/* navbar on smaller screens */}
-        <div className=" md:hidden">
+        <div className="md:hidden">
           <Bars3Icon
             className={`h-8 w-8 cursor-pointer text-zinc-900 transition ease-in-out ${
               showMobile ? "rotate-180" : "rotate-0"
@@ -28,6 +28,11 @@ const Navbar = () => {
             onClick={() => setShowMobile((val) => !val)}
           />
         </div>
+        {showMobile && (
+          <div className="md:hidden flex flex-col absolute w-full top-12 right-0 bg-white gap-y-2 py-4">
+            {navItems}
+          </div>
+        )}
       </div>
     </nav>
   );
