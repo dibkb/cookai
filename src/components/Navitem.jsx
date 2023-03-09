f51d99405d2f31267b17f141639e4d76b032717e
dibkb <dibas9110@gmail.com>
2023-03-09 10:37:34 +0530

    navbar mobie responsive

--- src/components/Navitem.jsx
+++ src/components/Navitem.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 const Navitem = ({ id, item, mobile }) => {
+  let className;
+  if (mobile) {
+    className =
+      "select-none text-center text-xl w-full cursor-pointer text-cookTextDark font-medium py-4 rounded-md hover:bg-yellow-50";
+  } else {
+    className =
+      "select-non text-center w-full cursor-pointer text-cookTextDark font-medium text-base py-2 px-4 rounded-md hover:bg-yellow-50";
+  }
+
   return (
-    <nav
-      key={id}
-      className="text-center w-full cursor-pointer text-cookTextDark font-medium text-base py-2 px-4 rounded-md hover:bg-yellow-50"
-    >
+    <nav key={id} className={className}>
       {item}
     </nav>
   );
