1b66c6ea79e4a3fb46454345d30c6a0814b7ae6f
dibkb <dibas9110@gmail.com>
2023-03-10 10:44:10 +0530

    width fix

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,44 +10,47 @@ const Home = () => {
   };
   return (
     <main className="mt-16 px-4 lg:px-12 xl:px-24 2xl:px-64">
-      <div className={styles["heading"]}>
-        <h1 className="self-center w-full text-[2.7rem] md:text-[3.6rem] sm:max-w-2xl  md:max-w-3xl  text lg:text-[4.2rem] font-raleway font-semibold text-center lg:max-w-4xl mt-4">
-          The only platform you’ll ever need to stay healthy and fit.
-        </h1>
-        <small className={styles.underline__1}></small>
-        <small className={styles.underline__2}></small>
-      </div>
-      <section className="mt-8 text-center text-base text-cookTextLight leading-8 flex flex-col gap-4">
-        <p>Hello there !</p>
-        <p>
-          I’m Harshit , leading Cook.AI , a startup which aims to revolutionize
-          the way we , the humans consume food. We are bringing Artificial
-          Intelligence and Digital technologies to personalize diets. With
-          advancement of technology in almost every aspect of our life , it is
-          now a need to modernize the very fundamental part of our life ,
-          eating!
-        </p>
-        <p>
-          My friends , Diet is a very important part of our life , every
-          molecules that goes into our body decides how the body is going to
-          function. A healthy diet is the stepping stone for a beautiful life.
-          All of us have different nutritional requirements , so thus different
-          taste requirements. We want to assure , that our users form a
-        </p>
-        <p>Thank You</p>
-      </section>
-      <section className="flex flex-col mt-16">
-        <div className="max-w-xs mx-auto w-[60%]">
-          <Button text={"Join Now"} onClickHandler={joinButtonClickHandler} />
+      <div className="max-w-screen-2xl mx-auto">
+        <div className={styles["heading"]}>
+          <h1 className="self-center w-full text-[2.7rem] md:text-[3.6rem] sm:max-w-2xl  md:max-w-3xl  text lg:text-[4.2rem] font-raleway font-semibold text-center lg:max-w-4xl mt-4">
+            The only platform you’ll ever need to stay healthy and fit.
+          </h1>
+          <small className={styles.underline__1}></small>
+          <small className={styles.underline__2}></small>
         </div>
-        {/* donwn arrow */}
-        <section className={styles["downArrow__container"]}>
-          <ArrowLong className={styles["arrow"]} />
-          <Ellipse className={styles["ellipse"]} />
+        <section className="mt-8 text-center text-base text-cookTextLight leading-8 flex flex-col gap-4">
+          <p>Hello there !</p>
+          <p>
+            I’m Harshit , leading Cook.AI , a startup which aims to
+            revolutionize the way we , the humans consume food. We are bringing
+            Artificial Intelligence and Digital technologies to personalize
+            diets. With advancement of technology in almost every aspect of our
+            life , it is now a need to modernize the very fundamental part of
+            our life , eating!
+          </p>
+          <p>
+            My friends , Diet is a very important part of our life , every
+            molecules that goes into our body decides how the body is going to
+            function. A healthy diet is the stepping stone for a beautiful life.
+            All of us have different nutritional requirements , so thus
+            different taste requirements. We want to assure , that our users
+            form a
+          </p>
+          <p>Thank You</p>
         </section>
-        {/* Join now modal */}
-        {showModal && <JoinnowModal />}
-      </section>
+        <section className="flex flex-col mt-16">
+          <div className="max-w-xs mx-auto w-[60%]">
+            <Button text={"Join Now"} onClickHandler={joinButtonClickHandler} />
+          </div>
+          {/* donwn arrow */}
+          <section className={styles["downArrow__container"]}>
+            <ArrowLong className={styles["arrow"]} />
+            <Ellipse className={styles["ellipse"]} />
+          </section>
+          {/* Join now modal */}
+          {showModal && <JoinnowModal />}
+        </section>
+      </div>
     </main>
   );
 };
--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -6,23 +6,27 @@ import { Heart } from "../utils/icons";
 const Vision = () => {
   return (
     <div className="w-full px-4 lg:px-12 xl:px-24 2xl:px-64 pb-4 flex flex-col mt-24">
-      <Mainheading title="Our Vision" />
-      <img src={vision} alt="" className="max-w-4xl mx-auto w-[90%]" />
-      <p className="text-lg text-center mt-12 leading-10 font-medium text-cookTextDark">
-        Using our unlimited passion for technology, we aspire to Modernize the
-        food consumption and fitness culture of the people for maintaining
-        optimum health. We also look forward to create better infrastructure and
-        logistics services for farms and farmers in order to revolutionize the
-        Primary Sector of our country.
-      </p>
-      <div className="mx-auto flex flex-col md:flex-row mb-12 mt-12 gap-12 w-[80%] md:max-w-2xl">
-        <Button text={"Join our Watchlist"} />
-        <Button text={"Join Our Cause"} mode="outline" />
+      <div className="max-w-screen-2xl mx-auto">
+        <div className="w-fit mx-auto">
+          <Mainheading title="Our Vision" />
+        </div>
+        <img src={vision} alt="" className="max-w-4xl mx-auto w-[90%]" />
+        <p className="text-lg text-center mt-12 leading-10 font-medium text-cookTextDark">
+          Using our unlimited passion for technology, we aspire to Modernize the
+          food consumption and fitness culture of the people for maintaining
+          optimum health. We also look forward to create better infrastructure
+          and logistics services for farms and farmers in order to revolutionize
+          the Primary Sector of our country.
+        </p>
+        <div className="mx-auto flex flex-col md:flex-row mb-12 mt-12 gap-12 w-[80%] md:max-w-2xl">
+          <Button text={"Join our Watchlist"} />
+          <Button text={"Join Our Cause"} mode="outline" />
+        </div>
+        <span className="flex items-center gap-2 mx-auto mt-12 mb-4 select-none w-fit">
+          <p className="text-zinc-700">Launching soon, Stay tuned</p>
+          <Heart />
+        </span>
       </div>
-      <span className="flex items-center gap-2 mx-auto mt-12 mb-4 select-none">
-        <p className="text-zinc-700">Launching soon, Stay tuned</p>
-        <Heart />
-      </span>
     </div>
   );
 };
