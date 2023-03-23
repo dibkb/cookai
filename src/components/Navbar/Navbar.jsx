214b4d2f6ab5e5161a3f7a541984d084f700acdc
dibkb <dibas9110@gmail.com>
2023-03-23 20:37:17 +0530

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
 import Navitem from "../Navbar/Navitem";
 const Navbar = () => {
   const [showMobile, setShowMobile] = useState(false);
   return (
     <nav className="z-50 shadow-md fixed top-0 w-full px-4 lg:px-12 xl:px-24 2xl:px-64 bg-white pb-4">
       <div className="flex items-center justify-between mt-3">
-        <img src={logo} alt="" width={97} className="select-none" />
+        <Link to="/">
+          <img src={logo} alt="" width={97} className="select-none" />
+        </Link>
         <div className="hidden gap-10 md:flex">
           <Navitem />
         </div>
