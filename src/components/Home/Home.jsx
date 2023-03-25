9f7df891bf9d844ecfdb9f74104f3b7c397744fa
dibkb <dibas9110@gmail.com>
2023-03-25 13:56:26 +0530

    intro update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -16,26 +16,25 @@ const Home = ({ executeScroll }) => {
         <section className="mt-8 text-center text-base text-cookTextLight leading-8 flex flex-col gap-4">
           <p>Hello there !</p>
           <p>
-            I’m Harshit , leading Cook.AI , a startup which aims to
-            revolutionize the way we , the humans consume food. We are bringing
-            Artificial Intelligence and Digital technologies to personalize
-            diets. With advancement of technology in almost every aspect of our
-            life , it is now a need to modernize the very fundamental part of
-            our life , eating!
+            I’m Harshit, leading Cook.AI, a startup which aims to revolutionize
+            the way we, the humans consume food. We are bringing Artificial
+            Intelligence and Digital technologies to personalize diets. With
+            advancement of technology in almost every aspect of our life, it is
+            now a need to modernize the very fundamental part of our life,
+            eating!
           </p>
           <p>
-            My friends , Diet is a very important part of our life , every
+            My friends, Diet is a very important part of our life, every
             molecules that goes into our body decides how the body is going to
             function. A healthy diet is the stepping stone for a beautiful life.
-            All of us have different nutritional requirements , so thus
-            different taste requirements. We want to assure , that our users
-            form a habit of meal planning using our super simplified
-            application, that lets you decide what you eat based on your
-            personalized body requirements. With a database of 50,000 + Recipes
-            from around the world , create the perfect meal plan for the week
+            We will ensure that our users form a habit of meal planning using
+            our super simplified application, that lets you decide what you
+            shall eat based on your personalized nutritional and taste
+            requirements. With a database of 50,000 + Recipes from around the
+            world, Create the perfect meal plan for the week automatically
             tracking every nutritional values that you want your body to
-            consume. We will make sure that every ingredients of the recipes
-            you’ve in you meal plan is delivered to you.
+            consume. Every ingredients of the recipes in your meal plan will be
+            delivered to your doorstep!
           </p>
           <p>Thank You</p>
         </section>
